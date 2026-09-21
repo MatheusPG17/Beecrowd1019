ac4ec0ba294ece06201a323db3c3abad99e0caa4
Matheus Pereira Gonçalves Coelho <mpg9159@gmail.com>
2026-09-21 16:39:18 -0300

    add: comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
+        //variaveis
         int segundost, hora, minuto, segundo;
         
+        //entrada de dados
         segundost = leia.nextInt();
         
+        //processamento
         hora = (segundost / 60) / 60;
         minuto = (segundost / 60) % 60;
         segundo = segundost % 60;
         
+        //saida de dados
         System.out.println(hora +":" + minuto + ":" + segundo);
     }
 }
